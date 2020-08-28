74aab2354c772d45592112bd6b1de1cf8a4c28ac
Hank Liu <hanke.liu@gmail.com>
2020-08-28 16:21:54 -0400

    verify components innerHTML testing passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
 export default () => {
-  return <div>Im the component</div>
-}
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
