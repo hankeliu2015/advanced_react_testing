c2120cfab2c16499971c18d8818652c0fd144236
Hank Liu <hanke.liu@gmail.com>
2020-08-28 10:54:06 -0400

    app component div element test passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.createElement('div');
+  ReactDom.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Hi there!');
+
+  ReactDom.unmountComponentAtNode(div);
 });
