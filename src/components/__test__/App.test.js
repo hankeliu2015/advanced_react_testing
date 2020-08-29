d760f66e3694d476b5a2d31094d6c4fd9b8ef47e
Hank Liu <hanke.liu@gmail.com>
2020-08-28 21:41:45 -0400

    shallow render test for comment list component

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
-import CommentBox from '../CommentBox'
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList'
 
 it('show a comment box', () => {
   const wrapped = shallow(<App />)
   expect(wrapped.find(CommentBox).length).toEqual(1);
+})
 
+it('show a comment list', () => {
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentList).length).toEqual(1);
 })
