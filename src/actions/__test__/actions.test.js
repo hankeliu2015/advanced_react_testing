3f579f6bb327455adbd860c18ac11797852d3971
Hank Liu <hanke.liu@gmail.com>
2020-09-03 11:57:58 -0400

    change root component to class component

--- src/actions/__test__/actions.test.js
+++ src/actions/__test__/actions.test.js
@@ -2,12 +2,12 @@ import { saveComment } from '../index.js';
 import { SAVE_COMMENT } from '../types.js';
 
 describe("action test", () => {
-  it('action has correct type', () => {
+  it('action has a type save comment', () => {
     let newAction = saveComment();
     expect(newAction.type).toEqual(SAVE_COMMENT);
   });
 
-  it('action has correct payload', () => {
+  it('action has correct payload with text', () => {
     let newAction = saveComment('my comments');
     expect(newAction.payload).toEqual("my comments");
   });
--- src/root.js
+++ src/root.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-export default (props) => {
-  return (
-    <Provider store={createStore(reducers, {})}>
-      {props.children}
-    </Provider>
-  )
+// export default (props) => {
+//   return (
+//     <Provider store={createStore(reducers, {})}>
+//       {props.children}
+//     </Provider>
+//   )
+// }
+
+class Root extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers, {})}>
+        {this.props.children}
+      </Provider>
+    )
+  }
 }
+
+export default Root;
