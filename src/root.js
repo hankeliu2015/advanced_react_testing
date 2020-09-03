fd7afd6bc6282c2d59f6d17b9973a5d3d1f4d954
Hank Liu <hanke.liu@gmail.com>
2020-09-03 16:00:32 -0400

    add cherrio query for test

--- src/root.js
+++ src/root.js
@@ -3,22 +3,21 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-// export default (props) => {
-//   return (
-//     <Provider store={createStore(reducers, {})}>
-//       {props.children}
-//     </Provider>
-//   )
-// }
-
-class Root extends Component {
-  render() {
-    return (
-      <Provider store={createStore(reducers, {})}>
-        {this.props.children}
-      </Provider>
-    )
-  }
+export default ({children, intialState={}}) => {
+  return (
+    <Provider store={createStore(reducers, intialState)}>
+      {children}
+    </Provider>
+  )
 }
 
-export default Root;
+// class Root extends Component {
+//   render() {
+//     return (
+//       <Provider store={createStore(reducers, {})}>
+//         {this.props.children}
+//       </Provider>
+//     )
+//   }
+// }
+// export default Root;
