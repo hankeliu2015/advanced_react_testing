ddbac42e41e482b2cca87c9fdf0a72707b0cfc13
Hank Liu <hanke.liu@gmail.com>
2020-09-03 21:07:52 -0400

    create a skekelton component for comments integration test

---
+++ src/__test__/commentsIntegration.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Root from './root';
+import { mount } from 'enzyme';
+import App from './components/App';
+
+it('fetch a list of comments and render them', () => {
+  //render the entire app
+  // find the fetch button and click on it
+  // find a list of comments.
+  
+})
